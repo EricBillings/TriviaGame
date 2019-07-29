da110a5730f96e0aa7aa93217855c84635d67903
Eric Billings <billings.company@gmail.com>
2019-07-29 09:30:04 -0400

    logging answer clicks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,24 @@
 let count = 0;
+let correct = 0;
+let incorrect = 0;
 
 let qa = {
 
-    1: ["Question 1", "Answer 1"],
-    2: ["Question 2", "Answer 2"],
-    3: ["Question 3", "Answer 3"],
-    4: ["Question 4", "Answer 4"],
-    5: ["Question 5", "Answer 5"]
+    1: ["Question 1", "Answer 1A", "Answer 1B", "Anwser 1C", "A"],
+    2: ["Question 2", "Answer 2A", "Answer 2B", "Answer 2C", "B"],
+    3: ["Question 3", "Answer 3A", "Answer 3B", "Answer 3C", "C"],
+    4: ["Question 4", "Answer 4A", "Answer 4B", "Answer 4C", "A"],
+    5: ["Question 5", "Answer 5A", "Answer 5B", "Answer 5C", "B"]
 
 }
 
-let questions = ["Q1", "Q2", "Q3"]
 
 $("#start").click(startGame);
 $("#stop").click(displayEnd);
+$(".choices").on("click", function () {
+    console.log($(this).attr("value"));
+})
+
 
 
 function displayEnd() {
@@ -26,6 +31,10 @@ function nextQuestion() {
     count++;
     if (count < 6) {
         $("#display").html(qa[count][0]);
+        $("#answer-a").html(qa[count][1]);
+        $("#answer-b").html(qa[count][2]);
+        $("#answer-c").html(qa[count][3]);
+
     } else {
         clearInterval(showQuestion);
         setTimeout(displayEnd, 3000);
